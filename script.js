b925dd679ef9bf79a0379f47a578d387bf12e598
damirgabdulin971-ai <damir.gabdulin971@gmail.com>
2025-08-23 01:07:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function initMap() {
     const salonLocation = { lat: 55.7558, lng: 37.6173 }; // Пример координат (Москва)
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const map = new api-maps.yandex.ru(document.getElementById("map"), {
         zoom: 15,
         center: salonLocation,
     });
-    const marker = new yandex.ru/maps({
+    const marker = new yandex.ru/maps/-/CHxPaM2D
         position: salonLocation,
         map: map,
         title: "Наш маникюрный салон",
     });
 }
+
 
