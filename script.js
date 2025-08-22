fc60c52106cbb6b57aa5f7d2ae354c790c2fbf37
damirgabdulin971-ai <damir.gabdulin971@gmail.com>
2025-08-23 01:28:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function initMap() {
     const salonLocation = { lat: 55.194984, lng: 61.308473 }; 
-    const map = <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A88c5b7ec0199e70daa1af8c1347ed46aaf674c69b183d6bd5183110dc1c3624a&amp;width=769&amp;height=666&amp;lang=ru_RU&amp;scroll=true"></script>
-        zoom: 15,
-        center: salonLocation,
+    const map = new yandex.ru/map-widget/v1/?um=constructor%3A88c5b7ec0199e70daa1af8c1347ed46aaf674c69b183d6bd5183110dc1c3624a&amp;source=constructor" width="769" height="666" frameborder="0"></iframe>
     });
     const marker = new yandex.ru/maps/-/CHxPaM2D
         position: salonLocation,
@@ -10,6 +8,7 @@ function initMap() {
         title: "Наш маникюрный салон",
     });
 }
+
 
 
 
